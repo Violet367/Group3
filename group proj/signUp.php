ee3fdcd8336b42f3359cd7e0df3ce017d4e1a763
violet367 <violet_gooding@yahoo.com>
2018-12-03 12:50:48 -0500

    Sign up

---
+++ group proj/signUp.php
@@ -0,0 +1,20 @@
+<?php
+
+  $email = $_POST["email"];
+  $password = $_POST["psw"];
+
+$servername = "localhost";
+$username = "root";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+ ?>

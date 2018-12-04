36b360d3cb06cef59a74981e7c2a36081941a8be
violet367 <violet_gooding@yahoo.com>
2018-12-03 23:30:13 -0500

    Database connected

--- group proj/signUp.php
+++ group proj/signUp.php
@@ -1,14 +1,15 @@
 <?php
 
   $email = $_POST["email"];
-  $password = $_POST["psw"];
+  $password1 = $_POST["psw"];
 
 $servername = "localhost";
 $username = "root";
-$password = "password";
+$password = "mysql";
+$database = "StudyBuddy";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
@@ -16,5 +17,14 @@ if ($conn->connect_error) {
 }
 
 echo "Connected successfully";
+
+$sql = "INSERT INTO Accounts (email, password) VALUES
+('$email', '$password1')";
+
+if(mysqli_query($conn, $sql)){
+    echo "Account created";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+}
 
  ?>

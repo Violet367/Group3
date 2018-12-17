5f6665c0364bf8fe94898ddfaefbd008109e74d0
ktaneja2 <ktaneja@umd.edu>
2018-12-16 23:11:19 -0500

    Fix config before push

--- group_proj/config.php
+++ group_proj/config.php
@@ -1,21 +1,17 @@
 <?php
 
-  $servername = "localhost";
-  $username = "root";
-  //$password = "In\$tE77-Dh1sch001";
-  $password = "";
-  $database = "StudyBuddy";
+$servername = "localhost";
+$username = "root";
+$password = "In\$tE77-Dh1sch001";/*"mysql";*/
+$database = "StudyBuddy";
 
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $database);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
 
-  // Check connection
-  if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-  }
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-  /*echo "Connected successfully";*/
-  if(session_id() == '' || !isset($_SESSION)) {
-    session_start(); // start sessions if it hasn't been started
-  }
+/*echo "Connected successfully";*/
 ?>
